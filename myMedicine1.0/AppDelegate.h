0e159e4d6660e15140a76f4baff2ebed8fd9e28d
linwcoder <770601773@q.com>
2016-07-13 15:49:36 +0800

    Initial Commit

---
+++ myMedicine1.0/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myMedicine1.0
+//
+//  Created by pollysoft on 16/7/13.
+//  Copyright © 2016年 microi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myMedicine1.0/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myMedicine1.0
+//
+//  Created by pollysoft on 16/7/13.
+//  Copyright © 2016年 microi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
